41a7ed65f2ede89d38e50f1f84add535a265909d
Alexander Su <alexanderytsu@gmail.com>
2017-03-02 02:43:47 +0000

    COMP1511 first commit

---
+++ COMP1511/lab1/face0.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+//face0
+int main(void){
+    printf("~ ~\n0 0\n o \n -");
+    return 0;
+}
---
+++ COMP1511/lab1/face1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+//face1
+int main(void){
+    printf("~ ~\n0 0\n o \n\\_/");
+    return 0;
+}
