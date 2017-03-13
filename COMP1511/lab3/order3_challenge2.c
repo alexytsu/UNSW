46e8fcc07ba98fcc40700177132e55ceb09373cb
Alexander Su <alexanderytsu@gmail.com>
2017-03-13 11:21:14 +0000

    COMP1511 I AM A GOD

--- COMP1511/lab3/order3_challenge2.c
+++ COMP1511/lab3/order3_challenge2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void){
+
     int a,b,c;
     printf("Enter integer: ");
     scanf("%d", &a);
@@ -10,34 +11,9 @@ int main(void){
     scanf("%d", &c);
 
 
-    int avg,diff,factor;
-    avg = (a+b);
-    diff = (avg-2*a)%avg;
-    factor = -1;
-    factor = factor + 2*(b>a);
-    diff = diff*factor;
-    a = (avg-diff)/2;
-    b = (avg+diff)/2;
-
-    avg = (b+c);
-    diff = (avg-2*b)%avg;
-    factor = -1;
-    factor = factor + 2*(c>b);
-    diff = diff*factor;
-    b = (avg-diff)/2;
-    c = (avg+diff)/2;
-    
-
-
-    
-    avg = (a+b);
-    diff = (avg-2*a)%avg;
-    factor = -1;
-    factor = factor + 2*(b>a);
-    diff = diff*factor;
-    a = (avg-diff)/2;
-    b = (avg+diff)/2;
-   
-    printf("The integers in order are: %d %d %d\n" ,a,b,c);
+    printf("The integers in order are: %d %d %d\n" ,
+    a*(a<b && a<c) + b*(b<a && b<c) + c*(c<a && c<b),
+    a*((a<b && a>c)||(a<c && a>b)) + b*((b<c && b>a)||(b>c && b<a)) + c*((c<a && c>b)||(c>a && c<b)),
+    a*(a>b && a>c) + b*(b>c&&b>a) + c*(c>a&&c>b));
 
 }
