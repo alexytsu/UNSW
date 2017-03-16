3593fe9e10890a0f0a18ba03d9a1d8afffa31e44
Alexander Su <alexanderytsu@gmail.com>
2017-03-16 02:14:27 +0000

    COMP1511 broken

--- COMP1511/lab3/order3_challenge2.c
+++ COMP1511/lab3/order3_challenge2.c
@@ -10,10 +10,9 @@ int main(void){
     printf("Enter integer: ");
     scanf("%d", &c);
 
-
     printf("The integers in order are: %d %d %d\n" ,
-    a*(a<b && a<c) + b*(b<a && b<c) + c*(c<a && c<b),
-    a*((a<b && a>c)||(a<c && a>b)) + b*((b<c && b>a)||(b>c && b<a)) + c*((c<a && c>b)||(c>a && c<b)),
-    a*(a>b && a>c) + b*(b>c&&b>a) + c*(c>a&&c>b));
+        a*(a<b && a<c) + b*(b<a && b<c) + c*(c<a && c<b),
+        a*((a<b && a>c)||(a<c && a>b)) + b*((b<c && b>a)||(b>c && b<a)) + c*((c<a && c>b)||(c>a && c<b)),
+        a*(a>b && a>c) + b*(b>c&&b>a) + c*(c>a&&c>b));
 
 }
