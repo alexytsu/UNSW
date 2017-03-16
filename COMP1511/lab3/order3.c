b64bad28d27df5e901a15ef3d31d32b147064f3c
z5162629 <z5162629@cse.unsw.edu.au>
2017-03-16 14:56:40 +1100

    COMP1511 lab03 completed

---
+++ COMP1511/lab3/order3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+
+    int a,b,c;
+    printf("Enter integer: ");
+    scanf("%d", &a);
+    printf("Enter integer: ");
+    scanf("%d", &b);
+    printf("Enter integer: ");
+    scanf("%d", &c);
+
+
+    printf("The integers in order are: %d %d %d\n" ,
+            a*(a<=b && a<=c) + b*(b<a && b<=c) + c*(c<a && c<b),
+            a*((a<=b && a>=c)||(a<=c && a>=b)) + b*((b<c && b>a)||(b>=c && b<a)) + c*((c<a && c>b)||(c>a && c<b)),
+            a*(a>=b && a>=c) + b*(b>=c&&b>a) + c*(c>a&&c>b));
+
+}
