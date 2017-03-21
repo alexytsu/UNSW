19aeed1314cc52158da04e8e9d607525e93ab41a
Alexander Su <alexanderytsu@gmail.com>
2017-03-21 12:28:51 +0000

    COMP1511 major progress ytd:odd lines after midline

--- COMP1511/lab4/spiral.c
+++ COMP1511/lab4/spiral.c
@@ -1,43 +1,110 @@
 #include <stdio.h>
 
 int main(void){
-    
-    int size;
 
+    //Get the size parameter from the user
+    int size;
     printf("Enter size: ");
     scanf("%d", &size);
+    int midrow = (size-1)/2; 
+    //These nested loops will print through the entire grid
+    //with x corresponding to the row and y corresponding to the column
 
-    for(int x = 0; x < size; x += 1){
-        for (int y = 0; y < size; y += 1){
-            
-            if(x==0){ //first row
+    for(int x = 1; x <= size; x += 1){
+        for (int y = 1; y <= size; y += 1){
+
+            //here are conditions under which, it will always be an asterix
+            //top row, right border, bottom row, left border(excl row2)
+            if(x==1 || y==size || x==size || y==1&&x!=2&&x%2!=0&&(x%2==0)){
                 printf("*");
-            }else if(y==0 && x!=1){ //leftmost side
-                printf("*");
-            }else if(x==size-1){ //bottom side
-                printf("*");
-            }else if(y==size-1){ //rightmost side
-                printf("*");
-            }else if(x%2==1 && x>=3){ //alternate lines beginning after the first
-              
-                if(x<= size/2){
-                    if(y == x -3){
-                        printf("*%d",x);
-                    }else{
+            }
+
+            //even lines before midline
+            else if(x%2==0 && x <= midrow){
+
+                int stars = x - 1;
+                int starsprinted = 0;
+
+                //j counts the columns in this even line
+                for(int j = 1; j<=size; j += 1){
+                    if(starsprinted < stars/2 && j%2==1){
+                        printf("*");
+                        starsprinted += 1;
+                    }else if(starsprinted >= stars/2 && j%2==1){
+                        int yettoprint = stars - starsprinted;
+                        if((size-j)/2<yettoprint){
+                            printf("*");
+                            starsprinted += 1;
+                        }else{
+                            printf("-");
+                        }
+                    }
+                    else{
                         printf("-");
                     }
                 }
-                
-
+                y += size;
             }
+
+            //odd lines before midline
+            else if(x%2==1 && x<= midrow){
+                int dashes = x - 2;
+                int dashesprinted = 0;
+
+                //j counts the columns in this odd line
+                for(int j = 1; j<=size; j += 1){
+                    if(dashesprinted < dashes/2 && j%2==0){
+                        printf("-");
+                        dashesprinted += 1;
+                    }else if(dashesprinted >= dashes/2 && j%2==0){
+                        int yettoprint = dashes - dashesprinted;
+                        if((size-j)/2 + 1 <= yettoprint){
+                            printf("-");
+                            dashesprinted += 1;
+                        }else{
+                            printf("*");
+                        }
+                    }else{
+                        if(j!=2){
+                        printf("*");
+                        }
+                    }
+                }
+                y += size;
+            }
+
+            //odd lines after midline
+
+            //even lines after midline
+            else if(x%2==0){
+                int stars = size - x + 1;
+                int starsprinted = 0;
+                for(int j = 1; j<=size; j += 1){
+                    if(starsprinted < stars/2 && j%2==1){
+                        printf("*");
+                        starsprinted += 1;
+                    }else if(starsprinted >= stars/2 && j%2==1){
+                        int yettoprint = stars - starsprinted;
+                        if((size-j)/2<yettoprint){
+                            printf("*");
+                            starsprinted += 1;
+                        }else{
+                            printf("-");
+                        }
+                    }
+                    else{
+                        printf("-");
+                    }
+                }
+                    y += size;
+            }
+
+            //if not an asterix, print a blank
             else{
                 printf("-");
             }
-
-            if (y == size - 1){
-                printf("\n");
-            }
         }
+    //at the end of each row, print a newline
+    printf("\n");
     }
-
 }
