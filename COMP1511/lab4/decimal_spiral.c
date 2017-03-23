4e4f2d18ef4c3df133350e078c5def602fdfec62
Alexander Su <alexanderytsu@gmail.com>
2017-03-23 00:41:20 +0000

    COMP1511 decimal spiral

--- COMP1511/lab4/decimal_spiral.c
+++ COMP1511/lab4/decimal_spiral.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int getDecimal(int s, int l, int x, int y);
+
 int main(void){
 
     //get size of grid from user
@@ -36,11 +38,11 @@ int main(void){
     for(row = 1; row <= size/2+1; row++){
         for(col = 1; col <= size; col++){
             if(col <= size - row + 1 && row%2 != 0 && col >= row-2){
-                printf("*");
+                printf("%d", getDecimal(size, length, row, col));
             } else if (col <= size - row + 1 && row%2==0 && col >=  row -2){
                 printf("-");
             } else if (col%2!=0){
-                printf("*");
+                printf("%d", getDecimal(size, length, row, col));
             } else{
                 printf("-");
             }
@@ -63,12 +65,20 @@ int main(void){
         }//closes col loop
        printf("\n"); 
     }//closes row loop
-    
 }
 
-int getDecimal(int length, int x, int y){
+int getDecimal(int s, int l, int x, int y){
 
-for :w
-
-
+    if (x == 1){
+        return (l - y + 1) % 10;
+    }else if (y == s){
+        return (l - s + 1 - x + 1) % 10;
+    }else if(x == s + 1){
+        return (l - s + 1 - x      
+    }else{
+        return 0;
+    }
+    //s is (side length of the grid
+    //l is the length of the spiral
+    //x and y give the position of the coordinate 
 }
