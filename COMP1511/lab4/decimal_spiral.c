094e39306b9bb9cbeb3f716718c21b4b10c19b5d
Alexander Su <alexanderytsu@gmail.com>
2017-03-26 13:05:03 +0000

    COMP1511 almost working

--- COMP1511/lab4/decimal_spiral.c
+++ COMP1511/lab4/decimal_spiral.c
@@ -13,7 +13,6 @@ int main(void){
 
     //while testing, set grid to a value
     int size = 5;
-
     //get the length of the spiral from the size of the grid
     int length = size * 2;
     int s = size - 2;
@@ -21,7 +20,7 @@ int main(void){
         length += 2 * s;
         s -= 2;
     }
-
+    printf("%d\n", getDecimal(size, length, 2, 5));
     printf("%d\n", length);
     //assuming the spiral starts at 0
     //from the outside in, the digit should be
@@ -38,13 +37,13 @@ int main(void){
     for(row = 1; row <= size/2+1; row++){
         for(col = 1; col <= size; col++){
             if(col <= size - row + 1 && row%2 != 0 && col >= row-2){
-                //printf("*");
-                printf("%d", getDecimal(size, length, row, col));
+                printf("+");
+                //printf("%d", getDecimal(size, length, row, col));
             } else if (col <= size - row + 1 && row%2==0 && col >=  row -2){
                 printf("-");
             } else if (col%2!=0){
-                //printf("*");
-                printf("%d", getDecimal(size, length, row, col));
+                printf("*");
+                //printf("%d", getDecimal(size, length, row, col));
             } else{
                 printf("-");
             }
@@ -56,13 +55,13 @@ int main(void){
     for(row = size/2+2; row <= size; row++){
         for(col = 1; col <= size; col++){
             if (col>=size-row+1 && row%2!=0 && col<=row-1){
-                printf("*");
-                //printf("%d", getDecimal(size, length, row + 1, col));
+                //printf("*");
+                printf("%d", getDecimal(size, length, row, col));
             } else if (col>=size -row +1 && row%2==0 && col <= row -1){
                 printf("-");
             } else if (col%2!=0){
-                printf("*");
-                //printf("%d", getDecimal(size, length, row + 1, col));
+                //printf("*");
+                printf("%d", getDecimal(size, length, row, col));
             } else{
                 printf("-");
             }
@@ -74,35 +73,52 @@ int main(void){
 
 
 int getDecimal(int s, int l, int xpos, int ypos){
-
     int x = 1;
     int y = 1;
-    int travelledTotal = 0;
+    int travelledTotal = 0 ;
     int curSide = s;
     int curTravelled = 0;
     int dir = 0;
+    int sideTime = 0;
+    int secondTime = 0;
 
     while( (x != xpos) || (y != ypos)){
-        if(curTravelled < curSide){
-            if(dir==0){
-                y+=1;
-            }else if (dir ==1){
-                x+=1;
-            }
-        travelledTotal += 1; 
         curTravelled += 1;
+        if(curTravelled < curSide ){
+            if(dir%4==0){
+                y+=1;
+                travelledTotal += 1;
+            }else if (dir%4==1){
+                x+=1;
+                travelledTotal += 1;
+            }else if (dir%4==2){
+                y-=1;
+                travelledTotal += 1;
+            }else if (dir%4==3){
+                x-=1;
+                travelledTotal += 1;
+            }
         }
 
 
         else if(curTravelled == curSide){
             //change direction
-            if(dir==0){
-                dir= 1;
-            }
+            dir ++;
+//            printf("changed!%d,%d,%d\n",curSide, curTravelled, travelledTotal);
             curTravelled = 0;
-            if (curSide = s){
-                curSide -= 1;
+     
+            if (curSide == s){
+                sideTime ++;
+            }else if(secondTime){
+                curSide -= 2;
+                secondTime = 0;
+            }else{
+                secondTime = 1;
             }
+            if (sideTime == 3){
+                curSide -= 2;
+            }
+
         }
         
     }
@@ -120,6 +136,64 @@ int getDecimal(int s, int l, int xpos, int ypos){
     }*/
     //s is (side length of the grid
     //l is the length of the spiral
-    return (l - travelledTotal)%10;
-    //x and y give the position of the coordinate 
+    return (l - travelledTotal - 1)%10;
+ 
+//    int x = 1;
+//    int y = 1;
+//    int travelledTotal = 0;
+//    int curSide = s;
+//    int curTravelled = 0;
+//    int dir = 0;
+//    int secondTime = 0;
+//
+//    while( (x != xpos) || (y != ypos)){
+//        if(curTravelled < curSide - 1){
+//            if(dir%4==0){
+//                y+=1;
+//            }else if (dir%4==1){
+//                x+=1;
+//            }else if (dir%4==2){
+//                y-=1;
+//            }else if (dir%4==3){
+//                x-=1;
+//            }
+//    
+//        travelledTotal += 1; 
+//        curTravelled += 1;
+//        }
+//
+//        else if(curTravelled == curSide -1){
+//            //change direction
+//            dir ++;
+//
+//            curTravelled = 0;
+//
+//            if (curSide == s){
+//                curSide -= 1;
+//            }else if(secondTime == 0){
+//                secondTime = 1;
+//
+//            }else if(secondTime == 1){
+//                curSide -= 2;
+//                secondTime = 0;
+//            }
+//        }
+//        
+//    }
+//
+//    /*if (x == 1){
+//        return (l - (y - 1)) % 10;
+//    }else if (y == s){
+//        return (l - (y - 1 + x - 1)) % 10;
+//    }else if(x == s + 1){
+//        return (l + (y - 1 + x - 1)) % 10;
+//    }else if(y ==1 ){
+//        return (l - (y  - x )) % 10;     
+//    }else{
+//        return 0;
+//    }*/
+//    //s is (side length of the grid
+//    //l is the length of the spiral
+//    return (l - travelledTotal)%10;
+//    //x and y give the position of the coordinate 
 }
