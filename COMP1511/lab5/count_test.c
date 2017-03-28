fae1081b9cc6bc371b0ee2eb4bd39620f0924d23
Alexander Su <alexanderytsu@gmail.com>
2017-03-28 04:06:22 +0000

    COMP1511 added arkle counting

--- COMP1511/lab5/count_test.c
+++ COMP1511/lab5/count_test.c
@@ -10,12 +10,15 @@ int main(void) {
         printf("Could not read hidden sequence\n");
         return 1;
     }
+
     printf("Enter guess: ");
     if (read_tiles(guess) != 1) {
         printf("Could not read guess\n");
         return 1;
     }
+
     printf("%d farnarkles\n", count_farnarkles(hidden_sequence, guess));
+    printf("%d arkles\n", count_arkles(hidden_sequence, guess));
 
     return 0;
 }
--- COMP1511/lab5/farnarkle.h
+++ COMP1511/lab5/farnarkle.h
@@ -4,3 +4,4 @@
 int read_tiles(int tiles[N_TILES]);
 void print_tiles(int tiles[N_TILES]);
 int count_farnarkles(int sequence1[N_TILES], int sequence2[N_TILES]);
+int count_arkles(int sequence1[N_TILES], int sequence2[N_TILES]);
