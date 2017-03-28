1c341148023c007fb5c09ff14d54d173f7cc12e2
Alexander Su <alexanderytsu@gmail.com>
2017-03-28 05:17:12 +0000

    COMP1511 autoplayer structure created; ytd: implement player logic

--- COMP1511/lab5/farnarkle.h
+++ COMP1511/lab5/farnarkle.h
@@ -1,7 +1,10 @@
 #define N_TILES 4
 #define MAX_TILE 8
+#define MAX_TURNS 100
 
+void farnarkle_player(int turn, int previous_guesses[MAX_TURNS][N_TILES], int farnarkles[MAX_TURNS], int arkles[MAX_TURNS], int guess[N_TILES]);
 int read_tiles(int tiles[N_TILES]);
 void print_tiles(int tiles[N_TILES]);
 int count_farnarkles(int sequence1[N_TILES], int sequence2[N_TILES]);
 int count_arkles(int sequence1[N_TILES], int sequence2[N_TILES]);
+void create_random_tiles(int tiles[N_TILES]);
