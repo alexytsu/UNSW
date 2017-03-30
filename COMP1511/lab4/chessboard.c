40ffc2c3706c1ded07fd251d8b75c825a9db86f3
Alexander Su <alexanderytsu@gmail.com>
2017-03-30 02:17:26 +0000

    COMP1511 shortened

--- COMP1511/lab4/chessboard.c
+++ COMP1511/lab4/chessboard.c
@@ -3,17 +3,14 @@
 int main(void){
     int a;
     int b = 1;
-    int flip = 0;
     printf("Enter size: ");
     scanf("%d", &a);
     while(b<=a*a){
-        if(flip){
+        if(b%2==1){
             printf("*");
-            flip = 0;
         }
         else{
             flip = 1;
-            printf("-");
         }
         if(b%a == 0){
            printf("\n");
