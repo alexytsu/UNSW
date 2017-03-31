617861fc0724d56076533c24f448711cc951c427
Alexander Su <alexanderytsu@gmail.com>
2017-03-31 09:21:51 +0000

    COMP1511 second ai attempt

---
+++ COMP1511/lab5/farnarkle_ai2/count_arkles.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "farnarkle.h"
+
+// return number of arkles
+int count_arkles(int sequence1[N_TILES], int sequence2[N_TILES]) {
+    int counted[N_TILES] = {0};
+    int arkles = 0;
+
+    for(int i = 0; i < N_TILES; i++){
+        if(sequence1[i] == sequence2[i]){
+            counted[i] = 1;
+        }
+    }
+
+    for(int i = 0; i < N_TILES; i++){
+        for(int j = 0; j < N_TILES; j++){
+            if(sequence2[i] == sequence1[j] && counted[j] == 0 && sequence2[i] != sequence1[i]){
+                counted[j] = 1;
+                arkles += 1;
+                break;
+            }
+        }
+    }
+
+    return arkles;
+
+}
---
+++ COMP1511/lab5/farnarkle_ai2/farnarkle_io.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "farnarkle.h"
+
+int read_tiles(int tiles[N_TILES]) {
+    for(int i = 0; i < N_TILES; i++){
+        scanf("%d", &tiles[i]);
+    }
+
+    return 1;
+}
+
+void print_tiles(int tiles[N_TILES]) {
+    for(int i = 0; i < N_TILES; i++){
+        printf("%d ", tiles[i]);
+    }
+
+    printf("\n");
+}
