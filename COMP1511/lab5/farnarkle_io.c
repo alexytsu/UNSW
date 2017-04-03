25daab3e6d7f1834002ae83d2e8e9f9af1d14b27
Alexander Su <alexanderytsu@gmail.com>
2017-04-03 11:21:52 +1000

    COMP1511 fail

--- COMP1511/lab5/farnarkle_io.c
+++ COMP1511/lab5/farnarkle_io.c
@@ -3,9 +3,9 @@
 
 int read_tiles(int tiles[N_TILES]) {
     for(int i = 0; i < N_TILES; i++){
-        if(scanf("%d", &tiles[i])){
+        tiles[i] = MAX_TILES +1 ;
+        scanf("%d", &tiles[i]);
 
-        }else{
             return 0;
         }
 
