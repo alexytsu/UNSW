2eaf8ced48ec2f493849769b596967ad9d9fc6ea
Alexander Su <alexanderytsu@gmail.com>
2017-04-03 00:01:33 +0000

    COMP1511 fixed io?

--- COMP1511/lab5/farnarkle_io.c
+++ COMP1511/lab5/farnarkle_io.c
@@ -3,7 +3,11 @@
 
 int read_tiles(int tiles[N_TILES]) {
     for(int i = 0; i < N_TILES; i++){
-        scanf("%d", &tiles[i]);
+        if(scanf("%d", &tiles[i])){
+                
+        }else{
+            return 0;
+        }
     }
 
     return 1;
