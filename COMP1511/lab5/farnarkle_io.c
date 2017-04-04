fcd22dde2d4dd08e6c2cce5c5fc9175dea19edd5
Alexander Su <alexanderytsu@gmail.com>
2017-04-04 03:42:25 +0000

    COMP1511 devowel done

--- COMP1511/lab5/farnarkle_io.c
+++ COMP1511/lab5/farnarkle_io.c
@@ -2,22 +2,12 @@
 #include "farnarkle.h"
 
 int read_tiles(int tiles[N_TILES]) {
-    
+
     for(int i = 0; i < N_TILES; i++){
-<<<<<<< HEAD
-        tiles[i] = MAX_TILES + 1;
-        scanf("%d", &tiles[i])
-                
-            return 0;
-        
-=======
-        if(scanf("%d", &tiles[i])){
+        scanf("%d", &tiles[i]);
 
-        }else{
-            return 0;
-        }
 
->>>>>>> f8689045a980c58da1966e003c64fbcac536feca
+
     }
 
     return 1;
---
+++ COMP1511/lab6/devowel.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+    
+int main(void) {
+    int ch;
+
+    ch = getchar();
+
+    while (ch != EOF) {
+        if(ch!='a'&&ch!='e'&&ch!='i'&&ch!='o'&&ch!='u'){
+
+        printf("%c", ch);
+        }
+        ch = getchar();
+        
+    }
+    return 0;
+}
