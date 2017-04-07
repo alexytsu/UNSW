8e3f28299dc2d25658886b20ecc3ba7e6c5d90e0
Alexander Su <alexanderytsu@gmail.com>
2017-04-07 00:34:45 +0000

    COMP1511 caesar shift half done: ytd actually shift

--- COMP1511/lab6/caesar.c
+++ COMP1511/lab6/caesar.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LENGTH 1000
 
 int main(int c, char *argv[]){
 
+    //get the caesar shift width
+    int shift = atoi(argv[1]) % 26;
+    
+    while(1){
+        //get the message to be shifted
+        char message[MAX_LENGTH];
+        fgets(message, MAX_LENGTH, stdin);
 
+        //perform the caesar shift on the message
+        int length = strlen(message); 
+        for(int i = 0; i < length; i++){
+            char cur = message[i]; 
 
+            if(cur >= 'a' && cur <= 'z'){
+                cur = ();
+                putchar(cur);
+
+            }else if(cur >= 'A' && cur <= 'Z'){
+
+            }
+        }
+
+        //print the ciphered message
+        printf("%s", message);
+
+    }
     return 0;
 }
-
-
