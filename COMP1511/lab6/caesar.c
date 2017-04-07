ed78acc3286307f029c3f99571bba9a9f41ba1ad
Alexander Su <alexanderytsu@gmail.com>
2017-04-07 10:36:14 +1000

    COMP1511 lab progress

--- COMP1511/lab6/caesar.c
+++ COMP1511/lab6/caesar.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_LINE 1000
 
 int main(int c, char *argv[]){
 
+    int offset = atoi(argv[1]), i = 0; 
+    char input[MAX_LINE];
 
+    while(fgets(input, MAX_LINE, stdin) != NULL){
+        for(i = 0; i < MAX_LINE-1; i++){
+            input[i] = 'a';            
+
+        }
+        printf("%s", input);
+    }
 
     return 0;
 }
