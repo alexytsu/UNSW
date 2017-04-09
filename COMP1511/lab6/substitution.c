c1a30dc02f9a72e10b0aaa660bf4b5d0cd2f0a38
Alexander Su <alexanderytsu@gmail.com>
2017-04-09 09:23:54 +0000

    COMP1511 whitespace

--- COMP1511/lab6/substitution.c
+++ COMP1511/lab6/substitution.c
@@ -3,16 +3,12 @@
 #include <stdlib.h>
 #define MAX_LENGTH 1000
 
-
 int main(int argc, char *argv[]){
-
     char message[MAX_LENGTH];
     while(fgets(message, MAX_LENGTH, stdin)!=NULL){
         //get the message to be shifted
-
         int length = strlen(message); 
         for(int i = 0; i < length; i++){
-
             if(message[i] >= 'a' && message[i] <= 'z'){
                 message[i] = argv[1][message[i]-'a']; 
                 putchar(message[i]);
@@ -20,7 +16,6 @@ int main(int argc, char *argv[]){
                 message[i] = argv[1][message[i]-'A']-'a'+'A'; 
                 putchar(message[i]);
             }else{
-
                 putchar(message[i]);
             }
         }
