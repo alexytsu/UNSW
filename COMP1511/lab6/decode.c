513d28caf37b9e4656a2a79a06fc2383b9a1ac85
Alexander Su <alexanderytsu@gmail.com>
2017-04-09 23:50:24 +0000

    COMP1511 commented decode

--- COMP1511/lab6/decode.c
+++ COMP1511/lab6/decode.c
@@ -6,29 +6,48 @@
 
 int main(int argc, char *argv[]){
 
-    char message[MAX_LENGTH];
-    while(fgets(message, MAX_LENGTH, stdin)!=NULL){
-        int length = strlen(message); 
-        for(int i = 0; i < length; i++){
+    char codedmessage[MAX_LENGTH];
+        
+    //read the input into message
+    while(fgets(codedmessage, MAX_LENGTH, stdin)!=NULL){
 
-            if(message[i] >= 'a' && message[i] <= 'z'){
+        //get the length of the coded message
+        int length = strlen(codedmessage); 
+        char decrypted[length];
+
+        //loop through the message; while inside this loop, codedmessage[i]
+        //is the letter at position i in the untranslated message
+        for(int i = 0; i < length; i++){
+            
+            if(codedmessage[i] >= 'a' && codedmessage[i] <= 'z'){
+
+                //this for loop runs 26 times, which allows us to see into the
+                //cipher by accessing argv[1][j]
                 for(int j = 0; j < 26; j++){
-                    if(argv[1][j] == message[i]){
-                        message[i] = 'a' + j;
+                    //check if the letter from the message currently being 
+                    //analysed is at position j in the cipher
+                    if(argv[1][j] == codedmessage[i]){
+                        //when this if statement succeeds, then the letter is 
+                        //at position j in the cipher
+                        //note that ('a' + j) is the ASCII value of the original
+                        //unciphered character
+                        
+                        //so the decrypted message is j letters away from 'a'
+                        decrypted[i] = 'a' + j;
                         break;
                     }
                 }
-                putchar(message[i]);
-            }else if(message[i] >= 'A' && message[i] <= 'Z'){
+                putchar(decrypted[i]);
+            }else if(codedmessage[i] >= 'A' && codedmessage[i] <= 'Z'){
                 for(int j = 0; j < 26; j++){
-                    if(argv[1][j]+'A'-'a' == message[i]){
-                        message[i] = 'A' + j;
+                    if(argv[1][j]+'A'-'a' == codedmessage[i]){
+                        decrypted[i] = 'A' + j;
                         break;
                     }
                 }
-                putchar(message[i]);
+                putchar(decrypted[i]);
             }else{
-                putchar(message[i]);
+                putchar(codedmessage[i]);
             }
         }
     }
