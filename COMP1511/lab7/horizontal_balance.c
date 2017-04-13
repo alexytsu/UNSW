d163353a91abcd74cf9067c99ac3a89af9a9262e
Alexander Su <alexanderytsu@gmail.com>
2017-04-13 00:22:35 +0000

    COMP1511 horizontal balance gives wrong values

---
+++ COMP1511/lab7/horizontal_balance.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+double get_horizontal_balance(int height, int width, int pixels[height][width]){
+
+    /*
+    horizontal_balance = (column_sum/n_black_pixels + 0.5)/width
+        where
+           column_sum = sum of the column indices of all black pixels
+           n_black_pixels = number of black pixels
+           width = image width
+    */
+    double n_black_pixels = 0;
+    double column_sum = 0;
+
+    for(int row = 0; row < height; row++){
+        for(int col = 0; col < height; col++){
+            if(pixels[row][col]){
+                column_sum += col;
+                n_black_pixels++;
+            }
+
+        }
+    }
+    double horizontal_balance = (column_sum/n_black_pixels + 0.5)/(width+1);
+    return(horizontal_balance);
+
+
+}
