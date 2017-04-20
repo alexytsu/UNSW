3a8c35b9bba231e7a763e3a9c2ef22da7be45467
Alexander Su <alexanderytsu@gmail.com>
2017-04-20 04:11:52 +0000

    COMP1511 refactored with 3d array

--- COMP1511/assignments/captcha/crack_digit.c
+++ COMP1511/assignments/captcha/crack_digit.c
@@ -2,7 +2,7 @@
 #include "captcha.h"
 
 int main(int argc, char *argv[]) {
-    
+
     //read the digit, get it's bounding box and get its balance
     int height, width, start_row, start_column, box_width, box_height, holes;
     double h_balance, v_balance, density;
@@ -33,76 +33,73 @@ int main(int argc, char *argv[]) {
         //balance = horizontal center of gravity
         h_balance = get_horizontal_balance(box_height, box_width, box_pixels);
         v_balance = get_vertical_balance(box_height, box_width, box_pixels);
-        
+
         //density of digit in the bounding box 
         density = get_density(box_height, box_width, box_pixels);
         holes = get_holes(box_height, box_width, box_pixels);
 
         //get the density of each quadrant of the bounded digit
-        //since quad_width & height are integers, they will take the correct value whether 
-        //box_width and height are odd or even 
-        //N.B. when both box_width and box_height are odd, the bottom left quadrant will be 
-        //smalle than the top right by one pixel in each dimension
+        //when the bounding box has even dimensions the quadrant dimensions are easy
+        //however, when odd, we will have one pixel overlap in the middle
+        //this way we can have all quadrants to be the exact same size rather than havign some one pixel
+        //wider or taller than others
+        //this helps us initialize a 3d array of quadrants which helps us compact analysis code
         //q3 | q4
         //___|___
         //q1 | q2  
 
-        int lower_height = box_height/2;
-        int left_width = box_width/2;
-        int higher_height, right_width;
 
-        //if even, top and bottom height are same, otherwise, q4 is larger by one pixel in each dimension
+        int start_row = box_height/2; 
+        int start_col = box_width/2;
+        int quad_height, quad_width;
         if(box_height % 2 == 1){
-            higher_height = height/2 + 1;
+            quad_height = box_height/2 + 1;
         }else{
-            higher_height = height/2;
+            quad_height = box_height/2;
         }
         if(box_width % 2 == 1){
-            right_width = height/2 + 1;
+            quad_width = box_width/2 + 1;
         }else{
-            right_width = height/2;
+            quad_width = box_width/2;
+        }
+    
+        printf("box_height: %d, box_width: %d\n", box_height, box_width);
+        int quadrants[4][quad_height][quad_width];
+        int start_coords[4][2] = {{0,0},
+                                  {0, start_col},
+                                  {start_row,0},
+                                  {start_row, start_col}};
+
+
+        for(int quad = 0; quad < 4; quad++){
+            copy_pixels(box_height, box_width, box_pixels, start_coords[quad][0], start_coords[quad][1], quad_height, quad_width, quadrants[quad]);
         }
 
-        //create copies of the pixels so that we can perform analysis on each quadrant
-
-        int dimensions[4][2] = {{lower_height, left_width}, 
-                                {lower_height, right_width}, 
-                                {higher_height, left_width}, 
-                                {higher_height, right_width}};
-
-        int quadrant1[dimensions[0][0]][dimensions[0][1]];
-        int quadrant2[lower_height][right_width];
-        int quadrant3[higher_height][left_width];
-        int quadrant4[higher_height][right_width];
-
-        copy_pixels(box_height, box_width, box_pixels, 0, 0, lower_height, left_width, quadrant1);
-        copy_pixels(box_height, box_width, box_pixels, 0, left_width, lower_height, right_width, quadrant2);
-        copy_pixels(box_height, box_width, box_pixels, lower_height, 0, higher_height, left_width, quadrant3);
-        copy_pixels(box_height, box_width, box_pixels, lower_height, left_width, higher_height, right_width, quadrant4);
-
-        //put them in an array for easier analysis
-        int quadrants[4] = {quadrant1, quadrant2, quadrant3, quadrant4};
-
-        //get the density of each quadrant
-        int densities[4];
-        for(int i = 0; i < 4; i ++){
+        for(int quad = 0; quad < 4; quad++){
+            for(int i = quad_height-1; i >= 0; i--){
+                for(int j = 0; j < quad_width; j++){
+                    printf("%d", quadrants[quad][i][j]);
+                }
+                printf("\n");
+            }
+            printf("\n");
         }
 
+
+        if(holes == 2){
+            printf("8");
+        }else if(holes == 1){
+            if(v_balance >= 0.55){
+                printf("9");
+            }else if(v_balance <= 0.45){
+                printf("6");
+            }else{
+                printf("0");
+            }
+        }else if(h_balance >= 0.5){
+            printf("3");
+        }
+
+        return 0;
     }
-       
-    if(holes == 2){
-        printf("8");
-    }else if(holes == 1){
-        if(v_balance >= 0.55){
-            printf("9");
-        }else if(v_balance <= 0.45){
-            printf("6");
-        }else{
-            printf("0");
-        }
-    }else if(h_balance >= 0.5){
-        printf("3");
-    }
-
-    return 0;
 }
