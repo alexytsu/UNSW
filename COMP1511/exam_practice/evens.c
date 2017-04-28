fc3a45f1557068f46fa3215e106399fcd07b4261
Alexander Su <alexanderytsu@gmail.com>
2017-04-28 03:01:13 +0000

    COMP1511 some exam practise with reading from stdin

--- COMP1511/exam_practice/evens.c
+++ COMP1511/exam_practice/evens.c
@@ -6,11 +6,11 @@ int main(void){
     int num;
     int input[MAX];
     int counter = 0;
-    while(scanf("%d", &num) != -1){
+    while(scanf("%d", &num) != EOF){
         input[counter] = num;
         counter ++;
     }
-    printf("The even numbers are: "); 
+    
     for(int i = 0; i < counter; i ++){
         if(input[i] % 2 == 0){
             printf("%d ", input[i]);
