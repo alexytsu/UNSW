bf82e7324540daf4a1577349cc28244a22d2c142
Alexander Su <alexanderytsu@gmail.com>
2017-05-06 13:43:20 +0000

    COMP1511 more efficient tail? autotest will tell...

--- COMP1511/lab9/tail.c
+++ COMP1511/lab9/tail.c
@@ -5,13 +5,19 @@ int main(int argc, char *argv[]){
 
     char *filename = argv[1];
     FILE *fp;
+    FILE *fin;
     fp = fopen(filename, "r");
 
     char buffer[10][MAX_LINE];
+    for(int i = 0; i < 10; i ++){
+        buffer[i][0] = -1;
+    }
     char cur_line[MAX_LINE];
     int lines = 0;
+
     while(fscanf(fp, "%s", cur_line)!=EOF){
         lines ++; 
+        /*
         for(int i = 0; i < MAX_LINE; i ++){
 
             buffer[9][i] = buffer[8][i];
@@ -25,19 +31,28 @@ int main(int argc, char *argv[]){
             buffer[1][i] = buffer[0][i];
             buffer[0][i] = cur_line[i];
         }
+        */
     }
-    fclose(fp);
 
-    if(lines >= 10){
-        for(int i = 0; i < 10; i ++){
-            printf("%s\n", buffer[9 - i]);
+    fclose(fp);
+    fin = fopen(filename, "r");
+    while(fscanf(fin, "%s", cur_line)!=EOF){
+        if(lines<=10){
+            for(int i = 0; i < MAX_LINE; i ++){
+                buffer[lines-1][i] = cur_line[i];
+            }
         }
-    }else{
-        int starting_index = lines - 1;
-        for(int i = starting_index; i >= 0; i ++){
+        lines--;
+    }
+
+    fclose(fin);
+
+    for(int i = 0; i < 10; i ++){
+        if(buffer[i][0] != -1){
             printf("%s\n", buffer[i]);
         }
     }
+
 
     return 0;
 }
