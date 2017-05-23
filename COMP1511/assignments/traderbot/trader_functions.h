e7c051ecfccdd722cd0c0c6ae44867b6c17b8e53
Alexander Su <alexanderytsu@gmail.com>
2017-05-23 18:03:40 +1000

    COMP1511 fixed market list for larger worlds

--- COMP1511/assignments/traderbot/trader_functions.h
+++ COMP1511/assignments/traderbot/trader_functions.h
@@ -3,96 +3,106 @@
 
 typedef struct location Location;
 
-typedef struct buyer{
+typedef struct store{
     int distance;
     int price;
-    int amount; 
+    int amount;
+    int type;
     struct buyer *next;
-}Buyer;
+}Store;
 
 typedef struct market{
     char *product;
     int supply;
-    int demand;   
+    int demand;
     int sellers;
     int buyers;
     int volume;
-    int weight;
-    //Buyer *buyer_list;
-    //Seller *seller_list;
+    int weight; 
+    Store *seller_list;
     struct market *next;  
 }Market;
 
-
-
-void market_from_location(Market *m, Location *loc){
-    m->product = loc->commodity->name;
+int market_from_location(Market *m, Location *loc){
     if(loc->type == LOCATION_BUYER){
         m->demand = loc->quantity;
         m->buyers = 1;
         m->supply = 0;
         m->sellers = 0;
+        m->product = loc->commodity->name;
+        m->volume = loc->commodity->volume;
+        m->weight = loc->commodity->weight;
+        m->next = NULL;
+        return 1;
     }
     else if(loc->type == LOCATION_SELLER){
         m->supply = loc->quantity;
         m->sellers = 1;
         m->buyers = 0;
         m->demand = 0;
+        m->product = loc->commodity->name;
+        m->volume = loc->commodity->volume;
+        m->weight = loc->commodity->weight;
+        m->next = NULL;
+        return 1;
     }
     else{
         m->supply = 0;
         m->sellers = 0;
         m->demand = 0;
         m->buyers = 0;
+        return 0;
     }
-    m->volume = loc->commodity->volume;
-    m->weight = loc->commodity->weight;
-    
-    //todo
-    //m->buyer_list = NULL;
-    //m->seller_list = NULL;
-    m->next = NULL;
 }
 
 void print_market_node(Market *m){
     printf("%s, supply: %d\t demand: %d\t sellers: %d\t buyers %d\t volume:%d\t weight:%d\t\n", m->product, m->supply, m->demand, m->sellers, m->buyers, m->volume, m->weight);
 }
 
-Market *market_add(Market *temp, Market *head){
-    printf("REACHED BEFORE ASSIGNMENTS!\n");
-    for(;head!=NULL;head=head->next){
-        printf("Entered loop iteration!\n");
-        if(!strcmp(head->product,temp->product)){
-            printf("Entered if statement\n");
-            head->supply+=temp->supply;
-            head->demand+=temp->demand;
-            head->sellers+=temp->sellers;
-            head->buyers+=temp->buyers;
-            head->volume=temp->volume;
-            head->weight=temp->weight;
+Market *market_add(Market *node, Market *head){
+    Market *previous = NULL;
+    Market *n = head;
+    for(;n!=NULL;n=n->next){
+        if(!strcmp(n->product,node->product)){
+            n->supply+=node->supply;
+            n->demand+=node->demand;
+            n->sellers+=node->sellers;
+            n->buyers+=node->buyers;
+            n->volume=node->volume;
+            n->weight=node->weight;
             return head;
         }
-        printf("Skipped if statement!\n");
+        previous = n;
     }
-    printf("New value!\n");
 
-    temp->next = head;
-    return temp; 
+    if(previous==NULL){
+        head = node;
+    }else{
+        previous->next = node;
+    }
+
+    node->next = n;
+
+    return head; 
 }
 
 Market *get_market_list(Location *cur_loc){
-    printf("#################\n\n");
     Location *start = cur_loc;
+
     Market *market_head = NULL;
+
     do{
-        Market *temp = malloc(sizeof(Market));
-        market_from_location(temp, cur_loc);
-        if(temp->buyers == 1 || temp->sellers == 1){
-            market_head = market_add(temp, market_head);
+        Market *temp = malloc(sizeof(struct market));
+        if(market_from_location(temp, cur_loc)){
+            print_market_node(temp);
+            if(temp->buyers == 1 || temp->sellers == 1){
+                market_head = market_add(temp, market_head);
+            }
         }
         cur_loc = cur_loc->next;
     }while(cur_loc != start);
 
+    printf("ALL NODES ADDED, PRINT THE WHOLE LIST\n");
     for(;market_head!=NULL;market_head = market_head->next){
         print_market_node(market_head);
     }
