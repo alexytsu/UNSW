2bfb4f476b2baac0604018c087e995fad083200d
Alexander Su <alexanderytsu@gmail.com>
2017-05-23 15:33:51 +1000

    COMP1511 fixed name

--- COMP1511/assignments/traderbot/p1zzz3r.c
+++ COMP1511/assignments/traderbot/p1zzz3r.c
@@ -7,7 +7,7 @@ void get_action(struct bot *bot, int *action, int *n);
 
 //Sets name as per assignment specs
 char *get_bot_name(void){
-    return "supr3m3 m3m3 p1zz3r cr1ng3"
+    return "supr3m3 m3m3 p1zz3r cr1ng3";
 }
 
 //the "main" function of this robot, it is called every turn and assigns values
