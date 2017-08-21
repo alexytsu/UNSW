d3a9a21d1d264a03995055fa2783a2bcfb5294bc
Alexander Su <alexanderytsu@gmail.com>
2017-08-22 01:01:48 +1000

    labs(COMP2521): cleanup

--- COMP2521/labs/week04/test.sh
+++ COMP2521/labs/week04/test.sh
@@ -1,19 +1,16 @@
 echo "Generating Test Data"
-for num in 10000 20000 50000 70000 100000 200000
+for NUM in 10000 20000 50000 70000 100000 200000
 do
-    for type in A D R
+    for TYPE in A D R
     do
-        for seed in 1000
-        do
-            ./gen $num $type $seed > $num_$type_$seed
+            ./gen $NUM $TYPE > "$NUM_$TYPE"
             echo "============================================================"
-            #printf "sortA\t %10d %5c %10d\t" $num $type $seed
-            #/usr/bin/time --format="%U seconds" ./sortA < $num_$type_$seed > /dev/null
-            printf "sortB\t %10d %5c %10d\t" $num $type $seed
-            /usr/bin/time --format="%U seconds" ./sortB < $num_$type_$seed > /dev/null
-            #printf "unix\t %10d %5c %10d\t" $num $type $seed
-            #/usr/bin/time --format="%U seconds" sort -n < $num_$type_$seed > /dev/null
-        done
+            printf "sortA\t %10d %5c %10d\n" $num $type
+            /usr/bin/time --format="%U seconds" ./sortA < $NUM_$TYPE > /dev/null
+            printf "sortB\t %10d %5c\n" $num $type
+            /usr/bin/time --format="%U seconds" ./sortB < $NUM_$TYPE > /dev/null
+            printf "unix\t %10d %5c \n" $num $type
+            /usr/bin/time --format="%U seconds" sort -n < $num_$TYPE > /dev/null
     done
 done
 
