1cf890f7991fbe6156ab72955073cfbbedbffc38
Alexander Su <alexanderytsu@gmail.com>
2017-10-20 10:48:05 +1100

    newTree header for ya

---
+++ COMP2521/assignments/searchEngine/newTree.h
@@ -0,0 +1,17 @@
+typedef struct treeNode{
+	char *url;
+    int matches;
+	double tfidf;
+	int deleted;
+	struct treeNode *left;
+	struct treeNode *right;
+}TreeRep;
+
+typedef TreeRep *Tree;
+Tree newTreeNode(char *name, double tfidf, int matches);
+Tree insertNode(Tree root, Tree insert);
+Tree insertNodeWithValues(Tree root, double tfidf, int matches, char *name);
+void deleteNode(Tree t);
+void printFromLargest(Tree root);
+void printTree(Tree root);
+int empty(Tree t);
