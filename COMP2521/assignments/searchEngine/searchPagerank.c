dde70768910cb4e9da78e6eeb4a4edee7ac769b2
Alexander Su <alexanderytsu@gmail.com>
2017-10-20 01:51:03 +1100

    removed binary file

--- COMP2521/assignments/searchEngine/searchPagerank.c
+++ COMP2521/assignments/searchEngine/searchPagerank.c
@@ -36,9 +36,9 @@ int getPageRanksIndexFromUrl(char *url, RankedResultEntry PageRanks[MAX_URLS])
 void printList(Entry list)
 {
     for(Entry curr = list; curr != NULL; curr = curr->next){
-        printf("%s[%.3lf]-> ", curr->url, curr->pageRank);
+        //printf("%s[%.3lf]-> ", curr->url, curr->pageRank);
     }
-    printf("x\n");
+    //printf("x\n");
 }
 
 Entry deleteEntry(Entry list, Entry toDelete){
@@ -63,8 +63,8 @@ Entry insertEntry(Entry head, Entry n)
 {
     Entry curr = head;
     Entry prev = NULL;
-    printf("\t\tInserting an entry for %s\n", n->url);
-    printf("\t\t\t the list about to be inserted into looks like this\n\t\t\t");
+    //printf("\t\tInserting an entry for %s\n", n->url);
+    //printf("\t\t\t the list about to be inserted into looks like this\n\t\t\t");
     printList(head);
 
     //if the list is empty
@@ -73,7 +73,7 @@ Entry insertEntry(Entry head, Entry n)
         return n;
     }
 
-    while(curr!=NULL && n->pageRank > curr->pageRank){
+    while(curr!=NULL && n->pageRank < curr->pageRank){
         prev = curr;
         curr = curr->next;
     }
@@ -91,6 +91,12 @@ int main(int argc, char *argv[])
 {
 
     int nSearchTerms = argc-1;
+    //printf("SEARCH TERMS\n");
+    for(int i = 1; i < argc; i ++){
+        //printf("%s ", argv[i]);
+    }
+
+    //printf("\n===============================\n");
 
     //an array of entries becomes our results
     Entry *results = malloc(sizeof(Entry) * nSearchTerms); 
@@ -99,9 +105,9 @@ int main(int argc, char *argv[])
     }
 
     FILE *f_index = fopen("invertedIndex.txt", "r");
-    printf("Successfully opened index\n");
+    //printf("Successfully opened index\n");
     FILE *f_pageRank = fopen("pagerankList.txt", "r");
-    printf("Successfully pagerank list\n");
+    //printf("Successfully pagerank list\n");
     RankedResultEntry *PageRanks = malloc(sizeof(RankedResultEntry) * MAX_URLS);
     char line[MAX_LINE_LENGTH];
 
@@ -112,7 +118,7 @@ int main(int argc, char *argv[])
         double pagerank;
         sscanf(line, "%s %d, %lf", url, &outlinks, &pagerank);
         url[strlen(url)-1] = '\0';
-        printf("%s %d %.7lf\n", url, outlinks, pagerank);
+        //printf("%s %d %.7lf\n", url, outlinks, pagerank);
         PageRanks[entries].url = malloc(sizeof(char) * 20);
         strcpy(PageRanks[entries].url, url);
         PageRanks[entries].pageRank = pagerank;
@@ -124,10 +130,10 @@ int main(int argc, char *argv[])
     while(fgets(line, MAX_LINE_LENGTH, f_index) != NULL){
         char *term = malloc(sizeof(char) * PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS);
         sscanf(line, "%s", term);
-        printf("Looking at index term %s\n", term);
+        //printf("Looking at index term %s\n", term);
         for(int i = 1; i <= nSearchTerms; i++){
             if(strcmp(term, argv[i]) == 0){
-                printf("\tMatched with a term in the index\n");
+                //printf("\tMatched with a term in the index\n");
                 
                 //here term is an entry in the index that matches a search term
                 //term is the first word in buffer "line" which holds all the urls containing the term
@@ -137,10 +143,10 @@ int main(int argc, char *argv[])
                     if(page[strlen(page)-1] == '\n'){
                         page[strlen(page)-1] = '\0';
                     }
-                    printf("\t\tPage: %s\n", page);
+                    //printf("\t\tPage: %s\n", page);
                     int index = getPageRanksIndexFromUrl(page, PageRanks); 
-                    printf("\t\t Got that boi index here %d\n", index);
-                    printf("\t\t Is it really tho? Let's check PageRanks[%d]: %s\n", index, PageRanks[index].url);
+                    //printf("\t\t Got that boi index here %d\n", index);
+                    //printf("\t\t Is it really tho? Let's check PageRanks[%d]: %s\n", index, PageRanks[index].url);
                     assert(index != -1);
                     Entry newPage = &PageRanks[index]; 
 
@@ -148,14 +154,14 @@ int main(int argc, char *argv[])
 
                     int matches = newPage->matches;
                     if(matches > 0){
-                        printf("PRINTING LIST TO DELETE STUFF FROM\n");
+                        //printf("PRINTING LIST TO DELETE STUFF FROM\n");
                         printList(results[matches-1]);
                         results[matches-1] = deleteEntry(results[matches-1], newPage);
-                        printf("PRINTING LIST AFTER DELETION\n");
+                        //printf("PRINTING LIST AFTER DELETION\n");
                         printList(results[matches-1]);
                     }
                     results[matches] = insertEntry(results[matches], newPage);
-                    printf("\t\t\tPRINTING AFTER INSERT\n\t\t\t");
+                    //printf("\t\t\tPRINTING AFTER INSERT\n\t\t\t");
                     printList(results[matches]);
                     newPage->matches++;
                     // =========================================================
@@ -164,11 +170,14 @@ int main(int argc, char *argv[])
         }
     }
 
-    for(int nmatched = 0; nmatched < nSearchTerms; nmatched ++){
-        printf("Number of terms matched: %d\n", nmatched + 1);
-        for(Entry curr = results[nmatched]; curr != NULL; curr = curr->next){
-            printf("%s: %.7lf\n", curr->url, curr->pageRank);
-        }
-    }
+    int entriesPrinted = 0;
 
+    for(int nmatched = nSearchTerms-1; nmatched >= 0; nmatched --){
+        for(Entry curr = results[nmatched]; curr != NULL; curr = curr->next){
+            entriesPrinted ++;
+            printf("%s\n", curr->url);
+            if(entriesPrinted == 30) break;
+        }
+        if(entriesPrinted == 30) break;
+    }
 }
