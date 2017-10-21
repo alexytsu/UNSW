b2027f29dde10378495c2680acb639f35959ffc2
dundun <sunjo726@gmail.com>
2017-10-22 09:59:33 +1100

    with brute force part 3, yet to write function that prints out final rankings but does print out sfr

--- COMP2521/assignments/searchEngine/bruteforce.c
+++ COMP2521/assignments/searchEngine/bruteforce.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <malloc.h>
 #include <math.h>
 #include <string.h>
@@ -14,7 +15,6 @@ int main(int argc, char *argv[]){
     char urls[MAX_URLS][20];
     int unionSize = 0;
     int rankTotal = 0;
-    char *filename = malloc(PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS*sizeof(char));
 
     
     List *fileLists = malloc((argc)*sizeof(List));  //contains urls from each file
@@ -23,15 +23,14 @@ int main(int argc, char *argv[]){
     int fileSize;  //amount of urls in each respective file
 
     while (fileCounter<=numberOfFiles){
-            strcpy(filename, argv[fileCounter]);
             fileSize = 0;
-            FILE *fin = fopen(filename, "r");  //for each file
+            FILE *fin = fopen(argv[fileCounter], "r");  //for each file
 
             fileLists[fileCounter-1] = newRatioList();   //make a new list for that file, counter-1 because array should start at 0
 
             while (fgets(urls[rankTotal], 20, fin)!=NULL){      //reads each line/rank of file into urls[]         
                 fileSize++;          //counts total ranks in specific file, also functions as set size for the rank file
-
+printf("sigh\n");
                 //Set
                 insertIntoRank(fileUnion, urls[rankTotal]);     //inserts url read into a union
                 //Linked List
@@ -40,7 +39,7 @@ int main(int argc, char *argv[]){
             }
             calculateRatio(fileLists[fileCounter-1], fileSize);    //assigns a ratio to each url in fileListss respective to their individual rank in that list
             fileCounter++;
-    
+    printf("sigh\n");
     }
     List allUrls = newRatioList();
     for(int listCounter=0; listCounter<numberOfFiles; listCounter++){
