b727e952b2952e894a913b0b193b146f1001351a
Alexander Su <alexanderytsu@gmail.com>
2018-03-07 13:38:31 +1100

    COMP1521 lab02 basically finished

--- COMP1521/020-Labs/lab02/BigNum.c
+++ COMP1521/020-Labs/lab02/BigNum.c
@@ -7,32 +7,87 @@
 #include <ctype.h>
 #include "BigNum.h"
 
+int max(int a, int b) {
+	return (a > b) ? a:b;
+}
+
 // Initialise a BigNum to N bytes, all zero
 void initBigNum(BigNum *n, int Nbytes)
 {
-   // TODO
-   return;
+	n->nbytes = Nbytes;
+	n->bytes= calloc(Nbytes, sizeof(Byte));
+	assert(n->bytes != NULL);
+	return;
 }
 
 // Add two BigNums and store result in a third BigNum
 void addBigNums(BigNum n, BigNum m, BigNum *res)
 {
-   // TODO
-   return;
+	int length = max(n.nbytes, m.nbytes);
+	length++;
+	if(res->nbytes < length) {
+		res->nbytes = length;
+		free(res->bytes);
+		res->bytes = calloc(length, sizeof(Byte));
+	}
+
+	int carryover = 0;
+
+	int i;
+	for(i = 0; i < length; i++){
+		int mdigit = 0;
+		int ndigit = 0;
+		if(i < m.nbytes) mdigit = m.bytes[i];
+		if(i < n.nbytes) ndigit = n.bytes[i];
+		int result = (mdigit) + (ndigit);
+		if(carryover) result++;
+		carryover = (result > 10);
+		res->bytes[i] = result % 10;
+	}
+
+	return;
 }
 
 // Set the value of a BigNum from a string of digits
 // Returns 1 if it *was* a string of digits, 0 otherwise
 int scanBigNum(char *s, BigNum *n)
 {
-   // TODO
-   return 1;
+	int length = strlen(s);
+	int i;
+	int j = 0;
+	while(s[j] == '0'){
+		j++;
+	}
+	for(i = length - 1; i >= 0; i--, j++){
+
+		if(s[j] <= '9' && s[j] >= '0'){
+			n->bytes[i] = s[j] - '0';
+		}else{
+			return 0;
+		}
+
+	}
+	return 1;
 }
 
 // Display a BigNum in decimal format
 void showBigNum(BigNum n)
 {
-   // TODO
-   return;
+    int i;
+    int flag = 0;
+    for(i = n.nbytes - 1; i >= 0; i--){
+ 		Byte curr = n.bytes[i];
+ 		if(flag == 0){
+ 			if(curr == 0){
+ 				continue;
+ 			}else{
+ 				flag = 1;
+ 				printf("%d", curr);
+ 			}
+ 		}else{
+ 			printf("%d", curr);
+ 		}
+    }
+	return;
 }
 
--- COMP1521/020-Labs/lab02/bits.c
+++ COMP1521/020-Labs/lab02/bits.c
@@ -6,33 +6,33 @@ int main()
 {
 	// Code to generate and display the largest "int" value
 
-	int x = ~0;
+	int x = ~0U >> 1;
 	printf("int %x, %d\n", x, x);
 
 	// Code to generate and display the largest "unsigned int" value
 
 	unsigned int y = ~0;
-	printf("unsigned int %x, %d\n", y, y);
+	printf("unsigned int %x, %u\n", y, y);
 
 	// Code to generate and display the largest "long int" value
 
-	long int xx = ~0;
-	printf("long int %x, %d\n", xx, xx);
+	long int xx = ~0UL >> 1;
+	printf("long int %lx, %ld\n", xx, xx);
 
 	// Code to generate and display the largest "unsigned long int" value
 
 	unsigned long int xy = ~0;
-	printf("unsigned long int %x, %d\n", xy, xy);
+	printf("unsigned long int %lx, %lu\n", xy, xy);
 
 	// Code to generate and display the largest "long long int" value
 
-	long long int xxx = ~0;
-	printf("long long int %x, %d\n", xxx, xxx);
+	long long int xxx = ~0ULL >> 1;
+	printf("long long int %llx, %lld\n", xxx, xxx);
 
 	// Code to generate and display the largest "unsigned long long int" value
 
 	unsigned long long int xxy = ~0;
-	printf("unsigned long long int %x, %d\n", xxy, xxy);
+	printf("unsigned long long int %llx, %llu\n", xxy, xxy);
 
 	return 0;
 }
