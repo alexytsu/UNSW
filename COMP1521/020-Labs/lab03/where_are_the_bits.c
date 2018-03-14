0f9776676e80b8243323e1652da1445fad216795
Alexander Su <alexanderytsu@gmail.com>
2018-03-14 13:40:54 +1100

    COMP1521 lab03: tidy up where_are_the_bits.c

--- COMP1521/020-Labs/lab03/where_are_the_bits.c
+++ COMP1521/020-Labs/lab03/where_are_the_bits.c
@@ -15,19 +15,16 @@ int main(void)
 {
 	struct _bit_fields x;
 
-	printf("%u\n",sizeof(x));
 	x.a = 0;
 	x.b = 0;
 	x.c = 1;
-
 	printf("a: %d\n", x.a);
 	printf("b: %d\n", x.b);
 	printf("c: %d\n", x.c);
 
 	void *a = &x;
-
 	printf("the int %d\n", *(int *)a);
-
+	
 	x.a = 1;
 	x.b = 0;
 	x.c = 0;
@@ -39,7 +36,6 @@ int main(void)
 	void *b = &x;
 
 	printf("the int %d\n", *(int *)b);
-
 
 	return 0;
 }
