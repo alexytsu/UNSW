bd972a833245551c3137120cccbb5586d053821e
Alexander Su <alexanderytsu@gmail.com>
2018-03-21 12:11:23 +1100

    COMP1521 lab04: clean begin

---
+++ COMP1521/020-Labs/lab04/fac2.s
@@ -0,0 +1,61 @@
+# COMP1521 18s2 Week 04 Lab
+# Compute factorials, iterative function
+
+
+### Global data
+
+   .data
+msg1:
+   .asciiz "n  = "
+msg2:
+   .asciiz "n! = "
+eol:
+   .asciiz "\n"
+
+### main() function
+   .text
+   .globl main
+main:
+   #  set up stack frame
+   sw    $fp, -4($sp)       # push $fp onto stack
+   la    $fp, -4($sp)       # set up $fp for this function
+   sw    $ra, -4($fp)       # save return address
+   sw    $s0, -8($fp)       # save $s0 to use as ... int n;
+   addi  $sp, $sp, -12      # reset $sp to last pushed item
+
+   #  code for main()
+   li    $s0, 0             # n = 0;
+   
+   la    $a0, msg1
+   li    $v0, 4
+   syscall                  # printf("n  = ");
+
+## ... rest of code for main() goes here ...
+ 
+   la    $a0, eol
+   li    $v0, 4
+   syscall                  # printf("\n");
+
+   # clean up stack frame
+   lw    $s0, -8($fp)       # restore $s0 value
+   lw    $ra, -4($fp)       # restore $ra for return
+   la    $sp, 4($fp)        # restore $sp (remove stack frame)
+   lw    $fp, ($fp)          # restore $fp (remove stack frame)
+
+   li    $v0, 0
+   jr    $ra                # return 0
+
+# fac() function
+
+fac:
+   # setup stack frame
+
+## ... code for prologue goes here ...
+
+   # code for fac()
+
+## ... code for fac() goes here ...
+
+   # clean up stack frame
+
+## ... code for epilogue goes here ...
